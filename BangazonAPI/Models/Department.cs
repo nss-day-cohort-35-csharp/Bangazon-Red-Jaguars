906ae28026d553309fa3ac8da55aed1ccb163528
Seth Oakley <sethoak@gmail.com>
2019-12-19 11:45:15 -0600

    saving department model

---
+++ BangazonAPI/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BangazonAPI.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(55, MinimumLength = 5, ErrorMessage = "Department names requires 5-55 characters")]
+        public string Name { get; set; }
+
+        [Required]
+        public int Budget { get; set; }
+
+    }
+}
