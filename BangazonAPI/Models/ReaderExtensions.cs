b635fc001aa87784c2fc25f62103e64e7a0209dd
Nate V <navogel@gmail.com>
2019-12-19 16:51:00 -0600

    completed computer controller

---
+++ BangazonAPI/Models/ReaderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public static class ReaderExtensions
+    {
+        public static DateTime? GetNullableDateTime(this SqlDataReader reader, string name)
+        {
+            var col = reader.GetOrdinal(name);
+            return reader.IsDBNull(col) ?
+                        (DateTime?)null :
+                        (DateTime?)reader.GetDateTime(col);
+        }
+    }
+}
