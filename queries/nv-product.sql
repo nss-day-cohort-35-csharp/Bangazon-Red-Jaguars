34f36b208b4b8d5f1d7407f6ba7e60adad08ca32
Nate V <navogel@gmail.com>
2019-12-19 14:41:43 -0600

    CRUD for product controller with recent/popular sorting and searching title/description

--- queries/nv-product.sql
+++ queries/nv-product.sql
@@ -1,6 +1,7 @@
-﻿SELECT  p.Id, pt.[Name] AS ProductType, p.CustomerId, p.Price, p.[Description], p.Title, p.DateAdded, COUNT(op.ProductId) FROM Product p
+﻿SELECT  p.Id, pt.[Name] AS ProductType, p.ProductTypeId, p.CustomerId, p.Price, p.[Description], p.Title, p.DateAdded, COUNT(op.ProductId) AS PopularityIndex FROM Product p
                                        
                                        LEFT JOIN ProductType pt ON p.ProductTypeId = pt.Id
                                        LEFT JOIN OrderProduct op ON p.Id = op.ProductId
-                                       GROUP BY p.Id, pt.[Name], p.CustomerId, p.Price, p.[Description], p.Title, p.DateAdded
-                                       
+                                      GROUP BY p.Id, p.ProductTypeId, pt.[Name], p.CustomerId, p.Price, p.[Description], p.Title, p.DateAdded
+                                        HAVING 1=1 AND (p.Title LIKE '%e%') OR (p.[Description] LIKE '%e%')
+                                        ORDER BY COUNT(op.ProductId) DESC
