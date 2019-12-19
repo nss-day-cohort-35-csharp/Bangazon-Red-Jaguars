33e1870c8f94a943e44a15349203798974b0f2a0
Nate V <navogel@gmail.com>
2019-12-19 11:43:34 -0600

    updated Query

--- queries/nv-product.sql
+++ queries/nv-product.sql
@@ -1,4 +1,6 @@
-﻿SELECT  p.Id, p.ProductTypeId, p.CustomerId, p.Price, p.[Description], p.Title, p.DateAdded, COUNT(op.ProductId) FROM Product p
+﻿SELECT  p.Id, pt.[Name] AS ProductType, p.CustomerId, p.Price, p.[Description], p.Title, p.DateAdded, COUNT(op.ProductId) FROM Product p
+                                       
+                                       LEFT JOIN ProductType pt ON p.ProductTypeId = pt.Id
                                        LEFT JOIN OrderProduct op ON p.Id = op.ProductId
-                                       GROUP BY p.Id, p.ProductTypeId, p.CustomerId, p.Price, p.[Description], p.Title, p.DateAdded
+                                       GROUP BY p.Id, pt.[Name], p.CustomerId, p.Price, p.[Description], p.Title, p.DateAdded
                                        
