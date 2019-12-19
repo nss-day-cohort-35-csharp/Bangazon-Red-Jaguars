4abca3fd1a1b164370b735ae869b5ddd51964b0b
Nate V <navogel@gmail.com>
2019-12-19 11:24:34 -0600

    added product model

---
+++ BangazonAPI/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public DateTime DateAdded { get; set; }
+        [Required]
+        public int ProductTypeId { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        [StringLength(255, MinimumLength = 3, ErrorMessage = "Titles require a minimum of 3 characters and a max of 255")]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(255, MinimumLength = 5, ErrorMessage = "Descriptions require a minimum of 5 characters and a max of 255")]
+
+
+        public string Description { get; set; }
+
+        public int CustomerId { get; set; }
+
+
+    }
+}
