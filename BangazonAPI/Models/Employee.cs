16d703ac7ae8ae115c87f4037496c9218f92e788
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-12-20 14:25:50 -0600

    added employee controller

--- BangazonAPI/Models/Employee.cs
+++ BangazonAPI/Models/Employee.cs
@@ -43,6 +43,6 @@ namespace BangazonAPI.Models
         [Required(ErrorMessage = "Computer Id is required")]
         public int ComputerId { get; set; }
 
-        public Computer Computer { get; set; }
+        public Computer? Computer { get; set; }
     }
 }
