796f3cd2987f295e27024a98519209b1702d265e
Seth Oakley <sethoak@gmail.com>
2020-01-06 14:52:48 -0600

    added CORS

--- BangazonAPI/Startup.cs
+++ BangazonAPI/Startup.cs
@@ -22,11 +22,23 @@ namespace BangazonAPI
             Configuration = configuration;
         }
 
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                builder =>
+                {
+                    builder.WithOrigins("http://bangazon.com");
+                });
+            });
+
             services.AddControllers();
 
             //add swagger code
@@ -56,6 +68,8 @@ namespace BangazonAPI
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseRouting();
 
