a9fd58c2eb891a5a3921c8c721eea71d5a5b7a4a
Seth Oakley <sethoak@gmail.com>
2020-01-06 14:05:44 -0600

    added RevenueReporting

---
+++ BangazonAPI/Models/RevenueReport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class RevenueReport
+    {
+        public List<ProductType> ProductTypes { get; set; } = new List<ProductType>();
+    }
+}
---
+++ RevenueReport.sql
@@ -0,0 +1,4 @@
+﻿SELECT p.Id, pt.Id AS ProductTypeId, p.Price, p.Title, pt.[Name], op.OrderId AS OrderId, o.UserPaymentTypeId AS PaymentType FROM ProductType pt
+LEFT JOIN Product p ON pt.id = p.ProductTypeId
+LEFT JOIN OrderProduct op ON op.ProductId = p.Id
+LEFT JOIN [Order] o ON o.Id = op.OrderId
