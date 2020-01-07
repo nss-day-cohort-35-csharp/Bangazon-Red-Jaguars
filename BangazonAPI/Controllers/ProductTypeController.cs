d602ded8e23acae5f2afdd49540524f0730e8754
Seth Oakley <sethoak@gmail.com>
2020-01-07 10:10:43 -0600

    removed using

--- BangazonAPI/Controllers/ProductTypeController.cs
+++ BangazonAPI/Controllers/ProductTypeController.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using System.Data;
 using BangazonAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Data.SqlClient;
